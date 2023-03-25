c20939e079f899a76982be8175b5c6ea26bcc663
litmus <74789224+Charleslit@users.noreply.github.com>
2023-03-25 10:13:12 +0300

    Update asset.go

--- asset/asset.go
+++ asset/asset.go
@@ -2,11 +2,21 @@ package asset
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"log"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
+)
+
+const (
+	maxIdleConnections = 5
+	maxOpenConnections = 10
+)
+
+var (
+	ErrAssetNotFound = errors.New("asset not found")
 )
 
 type Asset struct {
@@ -17,63 +27,71 @@ type Asset struct {
 	PurchaseDate     time.Time
 	PurchaseCost     float64
 	DepreciationRate float64
+	Depreciation     float64
 }
 
-func assets() {
-	db, err := sql.Open("mysql", "vms.sql")
-	if err != nil {
-		log.Fatalf("Failed to connect to database: %v", err)
-	}
-	defer db.Close()
-
-	assets, err := getAssets(db)
-	if err != nil {
-		log.Fatalf("Failed to get assets: %v", err)
-	}
-
-	updateAssets(db, assets)
-
-	generateMaintenanceReport(db)
-	generateDepreciationReport(db)
+type DBManager struct {
+	db *sql.DB
 }
 
-func getAssets(db *sql.DB) ([]Asset, error) {
-	rows, err := db.Query("SELECT id, name, description, serial_number, purchase_date, purchase_cost, depreciation_rate FROM assets")
+func NewDBManager(dsn string) (*DBManager, error) {
+	db, err := sql.Open("mysql", dsn)
 	if err != nil {
-		return nil, fmt.Errorf("Failed to retrieve assets: %v", err)
-	}
-	defer rows.Close()
-
-	assets := []Asset{}
-	for rows.Next() {
-		asset := Asset{}
-		err := rows.Scan(&asset.ID, &asset.Name, &asset.Description, &asset.SerialNumber, &asset.PurchaseDate, &asset.PurchaseCost, &asset.DepreciationRate)
-		if err != nil {
-			return nil, fmt.Errorf("Failed to scan asset: %v", err)
-		}
-		assets = append(assets, asset)
+		return nil, fmt.Errorf("failed to connect to database: %v", err)
 	}
 
-	return assets, nil
+	db.SetMaxIdleConns(maxIdleConnections)
+	db.SetMaxOpenConns(maxOpenConnections)
+
+	return &DBManager{db: db}, nil
 }
 
-func updateAssets(db *sql.DB, assets []Asset) error {
-	for _, asset := range assets {
-		depreciation := calculateDepreciation(asset)
+func (m *DBManager) Close() error {
+	return m.db.Close()
+}
 
-		stmt, err := db.Prepare("UPDATE assets SET depreciation = ? WHERE id = ?")
-		if err != nil {
-			return fmt.Errorf("Failed to prepare update statement: %v", err)
+func (m *DBManager) GetAssetByID(id int) (*Asset, error) {
+	row := m.db.QueryRow("SELECT id, name, description, serial_number, purchase_date, purchase_cost, depreciation_rate, depreciation FROM assets WHERE id = ?", id)
+
+	var asset Asset
+	err := row.Scan(&asset.ID, &asset.Name, &asset.Description, &asset.SerialNumber, &asset.PurchaseDate, &asset.PurchaseCost, &asset.DepreciationRate, &asset.Depreciation)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrAssetNotFound
 		}
-		defer stmt.Close()
 
-		_, err = stmt.Exec(depreciation, asset.ID)
-		if err != nil {
-			return fmt.Errorf("Failed to update asset record: %v", err)
-		}
-
-		fmt.Printf("Calculated depreciation for asset %d: %.2f\n", asset.ID, depreciation)
+		return nil, fmt.Errorf("failed to get asset: %v", err)
 	}
+
+	return &asset, nil
+}
+
+func (m *DBManager) UpdateAssetDepreciation(id int) error {
+	tx, err := m.db.Begin()
+	if err != nil {
+		return fmt.Errorf("failed to begin transaction: %v", err)
+	}
+
+	asset, err := m.GetAssetByID(id)
+	if err != nil {
+		tx.Rollback()
+		return fmt.Errorf("failed to get asset: %v", err)
+	}
+
+	depreciation := calculateDepreciation(*asset)
+
+	_, err = tx.Exec("UPDATE assets SET depreciation = ? WHERE id = ?", depreciation, id)
+	if err != nil {
+		tx.Rollback()
+		return fmt.Errorf("failed to update asset record: %v", err)
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return fmt.Errorf("failed to commit transaction: %v", err)
+	}
+
+	log.Printf("Calculated depreciation for asset %d: %.2f", id, depreciation)
 
 	return nil
 }
@@ -87,10 +105,10 @@ func calculateDepreciation(asset Asset) float64 {
 	return depreciation
 }
 
-func generateMaintenanceReport(db *sql.DB) {
+func (m *DBManager) GenerateMaintenanceReport() {
 	// Generate maintenance report logic here
 }
 
-func generateDepreciationReport(db *sql.DB) {
+func (m *DBManager) GenerateDepreciationReport() {
 	// Generate depreciation report logic here
 }
